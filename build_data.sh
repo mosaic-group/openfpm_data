b02ee9d1d321c41991239106ca409d4de1e1ebcf
Pietro Incardona <incardon@mpi-cbg.de>
2015-04-22 19:18:16 +0200

    Fixing build script

--- build_data.sh
+++ build_data.sh
@@ -6,6 +6,8 @@ mkdir /tmp/openfpm_data
 mv * .[^.]* /tmp/openfpm_data
 mv /tmp/openfpm_data OpenFPM_data
 
+mkdir OpenFPM_data/src/config
+
 git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git OpenFPM_devices
 
 cd "$1/OpenFPM_data"
