f346c5e1eaccaa8eb8e2c9711ca0836543d758e9
Pietro Incardona <incardon@mpi-cbg.de>
2015-05-04 23:37:02 +0200

    Workaround GCC 4.9.2

--- src/Grid/comb.hpp
+++ src/Grid/comb.hpp
@@ -166,4 +166,127 @@ struct comb
 
 };
 
+/*! brief specialization of comb in case of dim 0
+ *
+ *	GCC 4.9.2 does not accept structures of size 0
+ *
+ */
+
+template<>
+struct comb<0>
+{
+	//! FIX
+	char c[1];
+
+	/*! \brief check if it is a valid combination
+	 *
+	 * \return true if it is valid
+	 *
+	 */
+
+	inline bool isValid()
+	{
+		return true;
+	}
+
+	/*! \brief Check if the combination is a sub-element
+	 *
+	 * \param cmb combination to check if it is sub-element
+	 *
+	 * \return true if cmb is a sub-element of this combination
+	 *
+	 */
+
+	inline bool isSub(comb<0> cmb)
+	{
+		return true;
+	}
+
+	/*! \brief Set all the elements to zero
+	 *
+	 */
+
+	inline void zero()
+	{
+	}
+
+
+	/*! \brief Compare two combination
+	 *
+	 * check if they match
+	 *
+	 * \param t combination to check
+	 * \return true if the two combination match, false otherwise
+	 *
+	 */
+	inline bool operator!=(const comb<0> & t) const
+	{
+		// They match
+
+		return true;
+	}
+
+	/*! \brief Compare two combination
+	 *
+	 * check if they match
+	 *
+	 * \param t combination to check
+	 * \return true if the two combination match, false otherwise
+	 *
+	 */
+	inline bool operator==(const comb<0> & t) const
+	{
+		return true;
+	}
+
+	/*! \brief Get the i combination coefficient
+	 *
+	 * \param i coefficent
+	 * \return the coefficent of the combination at position i
+	 *
+	 */
+
+	inline char operator[](int i)
+	{
+		return 0;
+	}
+
+	/*! \brief get the combination array pointer
+	 *
+	 * \return an array of char representing the combination
+	 *
+	 */
+
+	inline char * getComb()
+	{
+		return c;
+	}
+
+	/*! \brief get the index i of the combination
+	 *
+	 * NOTE: used on expression template
+	 *
+	 * \param i index
+	 *
+	 * \return value of the i index
+	 *
+	 */
+	inline char value(int i) const
+	{
+		return c[i];
+	}
+
+
+	/* \brief It return the number of zero in the combination
+	 *
+	 * \return number of zero
+	 *
+	 */
+	inline int n_zero()
+	{
+		return 0;
+	}
+
+};
+
 #endif
