6c0d8f0e289b7280a25875341a3d27e728cbea7b
Pietro Incardona <incardon@mpi-cbg.de>
2015-06-26 22:40:28 +0200

    fixes script

--- build_io.sh
+++ build_io.sh
@@ -10,6 +10,9 @@ mkdir OpenFPM_IO/src/config
 
 git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git OpenFPM_devices
 git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_data.git OpenFPM_data
+cd "$1/OpenFPM_data"
+git checkout develop
+cd ..
 
 cd "$1/OpenFPM_IO"
 
