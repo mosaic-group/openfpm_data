07ec213511772b5fa16e5496105ac933e83a66c0
Pietro Incardona <incardon@mpi-cbg.de>
2015-06-26 22:22:39 +0200

    Fixing jenkin script

--- build_io.sh
+++ build_io.sh
@@ -11,7 +11,7 @@ mkdir OpenFPM_IO/src/config
 git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git OpenFPM_devices
 git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_data.git OpenFPM_data
 
-cd "$1/OpenFPM_data"
+cd "$1/OpenFPM_IO"
 
 sh ./autogen.sh
 if [ "$2" == "master" ]
