36684ff2099dc6bd8ab945298817244f45874cb6
Pietro Incardona <incardon@mpi-cbg.de>
2015-06-26 22:26:34 +0200

    Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define BOOST_DISABLE_ASSERTS
+
+
+#define BOOST_TEST_MODULE "C++ test module for OpenFPM_io project"
+#include <boost/test/included/unit_test.hpp>
+
+#include "CSVWriter_unit_tests.hpp"
+#include "GraphMLWriter_unit_tests.hpp"
+#include "VTKWriter_unit_tests.hpp"
