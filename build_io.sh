3884350fe4a94f19f8a84c43c9c314f41e126cef
Pietro Incardona <incardon@mpi-cbg.de>
2015-06-26 18:32:11 +0200

    Adding IO build script

---
+++ build_io.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Make a directory in /tmp/OpenFPM_data
+
+mkdir /tmp/openfpm_io
+mv * .[^.]* /tmp/openfpm_io
+mv /tmp/openfpm_io OpenFPM_IO
+
+mkdir OpenFPM_IO/src/config
+
+git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git OpenFPM_devices
+git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_data.git OpenFPM_data
+
+cd "$1/OpenFPM_data"
+
+sh ./autogen.sh
+if [ "$2" == "master" ]
+then
+ sh ./configure --disable-gpu
+else
+ sh ./configure
+fi
+make
+
+./src/io
+
