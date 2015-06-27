9557322e9e0efe198f2379f8052e06f4e2fe76e7
Pietro Incardona <incardon@mpi-cbg.de>
2015-06-27 10:29:23 +0200

    Fixing jenkins script

--- build_io.sh
+++ build_io.sh
@@ -23,6 +23,8 @@ then
  sh ./configure --disable-gpu
 elif [ "$2" == "gin" ]
 then
+ module load gcc/4.8.2
+ module load boost/1.54.0
  sh ./configure --with-boost=/sw/apps/boost/1.54.0/
 else
  sh ./configure
