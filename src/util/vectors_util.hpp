fe333d8136201ce9eaff68dd37942eab87c183fe
Yaroslav <beorn.90@gmail.com>
2015-07-23 19:26:55 +0200

    Weight counter

---
+++ src/util/vectors_util.hpp
@@ -0,0 +1,66 @@
+#define IS_GRID 2
+#define IS_VECTOR 1
+#define GRAPH
+// dim dimensionalility
+// T grid or vector
+// St type of space float double ....
+
+template <unsigned int dim, typename T, typename St, int impl = 2*is_grid<T>::value + is_vector<T>::value >
+class weight_counter
+{
+    size_t weight(T & t, const Box<dim,St> & domain, Box<dim,St> & b)
+    {     
+      //error if T is not a grid or a vector
+      if (is_grid<T>::value == false && is_vector<T>::value == false)
+          std::cerr << "Not implemented";
+        return 0;
+    }
+}
+
+/////// When T = openfpm::grid_cpu
+
+template <unsigned int dim, typename T, typename St>
+class weight_counter<dim,T,St,IS_GRID>
+{
+    size_t weight(T & t, Box<dim,T> & b)
+    {
+       // Grid implementation
+      // get a size vector
+      size_t a[dim] = t.getGrid().getSize();
+      size_t res = 1;
+      //for each dimension
+      for (int i = 0; i < dim; i++)
+      {
+        size_t k = a[i];
+        size_t cur = 0;
+        for (int j = 0; j < k; j++)
+          //check if the points on the sides of a grid are inside the box
+          if (b.getLow(i)*(k-1) <= j && j <= b.getHigh(i)*(k-1))
+            ++cur;
+        //multiply points inside the box on each side
+        res *= cur;
+      }
+      return res;
+    }
+}
+
+////// When T = openfpm::vector
+
+template <unsigned int dim, typename T, typename St>
+class weight_counter<dim,T,St,IS_VECTOR>
+{
+    size_t weight(openfpm::vector<Point<dim>> & t, const Box<dim,St> & domain, Box<dim,St> & b)
+    {
+       // Vector implementation
+      //get size of a vector = number of points
+      size_t size = t.size();
+      int count = 0;
+      //count points those are inside of a box
+      for (int i = 0; i < size, i++) 
+      {
+        if (b.isInside(t.get(i)))
+          count++;
+      }
+      return count;
+    }
+}
