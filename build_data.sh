fa8e3910fa2c7ad2e2b2e52234cb9b4c77195326
Pietro Incardona <incardon@mpi-cbg.de>
2015-09-14 00:11:20 +0200

    Fixing directory convention

--- build_data.sh
+++ build_data.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-# Make a directory in /tmp/OpenFPM_data
+# Make a directory in /tmp/openfpm_data
 
 mkdir /tmp/openfpm_data
 mv * .[^.]* /tmp/openfpm_data
 mv /tmp/openfpm_data openfpm_data
 
-mkdir OpenFPM_data/src/config
+mkdir openfpm_data/src/config
 
 git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git openfpm_devices
 
-cd "$1/OpenFPM_data"
+cd "$1/openfpm_data"
 
 sh ./autogen.sh
 if [ "$2" == "master" ]
