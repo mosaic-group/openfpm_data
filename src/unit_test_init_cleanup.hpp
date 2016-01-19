b0cab30f3c38279f6963a8185acee74925d28dbb
Pietro Incardona <i-bird@localhost.localdomain>
2016-01-19 16:20:04 -0500

    Adding missing file

---
+++ src/unit_test_init_cleanup.hpp
@@ -0,0 +1,37 @@
+/*
+ * unit_test_init_cleanup.hpp
+ *
+ *  Created on: Apr 17, 2015
+ *      Author: Pietro Incardona
+ */
+
+#ifndef UNIT_TEST_INIT_CLEANUP_HPP_
+#define UNIT_TEST_INIT_CLEANUP_HPP_
+
+struct ut_start
+{
+    ut_start()
+    {
+    	test_dir = getenv("OPENFPM_PERFORMANCE_TEST_DIR");
+
+    	if (test_dir == NULL)
+    	{
+    		std::cerr << "Error: " __FILE__ << ":" << __LINE__ << " in order to run the performance test you must set the environment variable $OPENFPM_PERFORMANCE_TEST_DIR to the test or an empty directory";
+    		return;
+    	}
+    }
+    ~ut_start()
+    {
+#ifdef PERFORMANCE_TEST
+    	write_performance_report();
+#endif
+    }
+};
+
+//____________________________________________________________________________//
+
+BOOST_GLOBAL_FIXTURE( ut_start );
+
+
+
+#endif /* UNIT_TEST_INIT_CLEANUP_HPP_ */
