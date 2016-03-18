88b06d9c79d49fa380c54a51e58a2497059c4cdf
Pietro Incardona <i-bird@localhost.localdomain>
2016-03-18 01:30:03 +0100

    Fixing io compilation

--- build_io.sh
+++ build_io.sh
@@ -25,9 +25,9 @@ elif [ "$2" == "gin" ]
 then
  module load gcc/4.8.2
  module load boost/1.54.0
- sh ./configure --with-boost=/sw/apps/boost/1.54.0/
+ sh ./configure --with-boost=/sw/apps/boost/1.54.0/ --with-hdf5=$HOME/HDF5/bin/h5pcc
 else
- sh ./configure
+ sh ./configure --with-hdf5=$HOME/HDF5/bin/h5pcc
 fi
 make
 
