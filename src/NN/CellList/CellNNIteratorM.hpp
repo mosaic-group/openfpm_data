8de11d9e562efff7142bc1630c38dc9d43c3f896
Pietro Incardona <incardon@mpi-cbg.de>
2017-01-27 17:00:49 +0100

    Fixing clang compilation

--- src/NN/CellList/CellNNIteratorM.hpp
+++ src/NN/CellList/CellNNIteratorM.hpp
@@ -145,7 +145,7 @@ public:
  * \tparam impl implementation specific options NO_CHECK do not do check on access, SAFE do check on access
  *
  */
-template<unsigned int dim, typename Cell, unsigned int sh_byte,unsigned int NNc_size, unsigned int impl> class CellNNIteratorM : public CellNNIterator<dim,Cell,NNc_size,impl>
+template<unsigned int dim, typename Cell, unsigned int sh_byte, int NNc_size, unsigned int impl> class CellNNIteratorM : public CellNNIterator<dim,Cell,NNc_size,impl>
 {
 	typedef boost::low_bits_mask_t<sizeof(size_t)*8-sh_byte>  mask_low;
 
