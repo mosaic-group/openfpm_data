308f98c53bec06da21084170c85808d3dbb609ef
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-02 18:45:22 +0100

    Fixing passing artifatcs to run stage

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-
+ls
 $pre_command ./src/mem_map
 if [ $? -ne 0 ]; then
     curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed the test with openfpm_data test $opt_comp \" }] }" https://hooks.slack.com/services/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
