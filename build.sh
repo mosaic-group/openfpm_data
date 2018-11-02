852f7c4a46af2088c7a7d05ef9e03a5f8111622a
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-02 18:49:37 +0100

    Fixing passing artifatcs to run stage

--- build.sh
+++ build.sh
@@ -20,8 +20,6 @@ if [ ! -d $HOME/openfpm_dependencies/openfpm_data/BOOST ]; then
 	fi
 fi
 
-ls $HOME/openfpm_dependencies/openfpm_data/BOOST/lib
-
 mkdir /tmp/openfpm_data_$3
 mv * .[^.]* /tmp/openfpm_data_$3
 mv /tmp/openfpm_data_$3 openfpm_data
@@ -63,3 +61,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+pwd
