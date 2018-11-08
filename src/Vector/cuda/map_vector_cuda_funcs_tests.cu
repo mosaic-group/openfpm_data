e21987398a248552a20ee803a529dc25513ea21f
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-08 18:05:32 +0100

    Adding missing files

---
+++ src/Vector/cuda/map_vector_cuda_funcs_tests.cu
@@ -0,0 +1,140 @@
+/*
+ * map_vector_cuda_funcs_tests.cu
+ *
+ *  Created on: Aug 17, 2018
+ *      Author: i-bird
+ */
+
+
+#define BOOST_GPU_ENABLED __host__ __device__
+
+#include "config.h"
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "util/cuda_util.hpp"
+#include "Vector/map_vector.hpp"
+#include "util/tokernel_transformation.hpp"
+
+BOOST_AUTO_TEST_SUITE( vector_cuda_funcs_tests )
+
+
+BOOST_AUTO_TEST_CASE( vector_cuda_funcs_add_prp_device )
+{
+	openfpm::vector_gpu<aggregate<float,float[3],float[3][3]>> vg_data;
+	openfpm::vector_gpu<aggregate<float,float[3],float[3][3]>> vg_data2;
+
+	vg_data.resize(100);
+	vg_data2.resize(100);
+
+	// we fill vg_data with something
+
+	for (size_t i = 0 ; i < 100 ; i++)
+	{
+		vg_data.template get<0>(i) = 2.5 + i;
+
+		vg_data.template get<1>(i)[0] = 4.6 + i;
+		vg_data.template get<1>(i)[1] = 7.8 + i;
+		vg_data.template get<1>(i)[2] = 9.0 + i;
+
+		vg_data2.template get<0>(i) = 8.5 + i;
+
+		vg_data2.template get<1>(i)[0] = 1.6 + i;
+		vg_data2.template get<1>(i)[1] = 3.8 + i;
+		vg_data2.template get<1>(i)[2] = 5.1 + i;
+	}
+
+	vg_data.hostToDevice<0,1>();
+	vg_data2.hostToDevice<0,1>();
+
+	vg_data.add_prp_device<aggregate<float,float[3],float[3][3]>,
+						   CudaMemory,
+						   openfpm::grow_policy_double,
+						   OPENFPM_NATIVE,
+						   memory_traits_inte,
+	                       0,1>(vg_data2);
+
+	vg_data.deviceToHost<0,1>();
+
+	BOOST_REQUIRE_EQUAL(vg_data.size(),200);
+
+	bool match = true;
+	for (unsigned int i = 100 ; i < 200 ; i++)
+	{
+		match &= vg_data.template get<0>(i) == vg_data2.template get<0>(i-100);
+
+		match &= vg_data.template get<1>(i)[0] == vg_data2.template get<1>(i-100)[0];
+		match &= vg_data.template get<1>(i)[1] == vg_data2.template get<1>(i-100)[1];
+		match &= vg_data.template get<1>(i)[2] == vg_data2.template get<1>(i-100)[2];
+	}
+
+	BOOST_REQUIRE_EQUAL(match,true);
+}
+
+BOOST_AUTO_TEST_CASE( vector_cuda_to_kernel_recursive2 )
+{
+	typedef openfpm::vector_gpu<aggregate<int,long int>> test1_type;
+	typedef openfpm::vector_gpu<aggregate<int,openfpm::vector_gpu<aggregate<long int>>>> test2_type;
+	typedef openfpm::vector_gpu<aggregate<int,openfpm::vector_gpu<aggregate<Box<2,float>>>>> test3_type;
+	typedef openfpm::vector<Box<3,float>,CudaMemory,typename memory_traits_inte<Box<3,float>>::type,memory_traits_inte> test4_type;
+
+	typedef typename toKernel_transform<memory_traits_inte,test1_type>::type tker1;
+	typedef typename toKernel_transform<memory_traits_inte,test2_type>::type tker2;
+	typedef typename toKernel_transform<memory_traits_inte,test3_type>::type tker3;
+	typedef typename toKernel_transform<memory_traits_inte,test4_type>::type tker4;
+
+	bool test = std::is_same<tker1,openfpm::vector_gpu_ker<aggregate<int, long>, memory_traits_inte>>::value;
+
+	BOOST_REQUIRE_EQUAL(test,true);
+
+	test = std::is_same<tker2,openfpm::vector_gpu_ker<aggregate<int, openfpm::vector_gpu_ker<aggregate<long>, memory_traits_inte> >, memory_traits_inte>>::value;
+
+	BOOST_REQUIRE_EQUAL(test,true);
+
+	test = std::is_same<tker3,openfpm::vector_gpu_ker<aggregate<int, openfpm::vector_gpu_ker<aggregate<Box<2,float>>, memory_traits_inte> >, memory_traits_inte>>::value;
+
+	BOOST_REQUIRE_EQUAL(test,true);
+
+	test = std::is_same<tker4,openfpm::vector_gpu_ker<Box<3,float>,memory_traits_inte>>::value;
+
+	BOOST_REQUIRE_EQUAL(test,true);
+}
+
+BOOST_AUTO_TEST_CASE( vector_cuda_to_cpu_operator_equal )
+{
+	openfpm::vector_gpu<aggregate<int,int,double>> v1;
+	openfpm::vector<aggregate<int,int,double>> v2;
+	openfpm::vector<aggregate<int,int,double>,HeapMemory,typename memory_traits_inte<aggregate<int,int,double>>::type, memory_traits_inte > v3;
+	openfpm::vector<aggregate<int,int,double>> v4;
+
+	v2.resize(3000);
+
+	for (size_t i = 0 ; i < 3000 ; i++)
+	{
+		v2.template get<0>(i) = i;
+		v2.template get<1>(i) = i+300;
+		v2.template get<2>(i) = i+6123.0;
+	}
+
+	v1 = v2;
+	v3 = v2;
+	v4 = v1;
+
+	for (size_t i = 0 ; i < v2.size() ; i++)
+	{
+		BOOST_REQUIRE_EQUAL(v2.template get<0>(i),v1.template get<0>(i));
+		BOOST_REQUIRE_EQUAL(v2.template get<0>(i),v3.template get<0>(i));
+		BOOST_REQUIRE_EQUAL(v2.template get<0>(i),v4.template get<0>(i));
+
+		BOOST_REQUIRE_EQUAL(v2.template get<1>(i),v1.template get<1>(i));
+		BOOST_REQUIRE_EQUAL(v2.template get<1>(i),v3.template get<1>(i));
+		BOOST_REQUIRE_EQUAL(v2.template get<1>(i),v4.template get<1>(i));
+
+		BOOST_REQUIRE_EQUAL(v2.template get<2>(i),v1.template get<2>(i));
+		BOOST_REQUIRE_EQUAL(v2.template get<2>(i),v3.template get<2>(i));
+		BOOST_REQUIRE_EQUAL(v2.template get<2>(i),v4.template get<2>(i));
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
