1c50460ae547bf8cb24f3b9e961ed78910fb0a5e
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-17 20:35:54 +0100

    Reinstall BOOST

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ if [ ! -d $HOME/openfpm_dependencies/openfpm_data/LIBHILBERT ]; then
 	./install_LIBHILBERT.sh $HOME/openfpm_dependencies/openfpm_data/ 4
 fi
 
+rm -rf $HOME/openfpm_dependencies/openfpm_data/BOOST
+
 if [ ! -d $HOME/openfpm_dependencies/openfpm_data/BOOST ]; then
 	if [ x"$2" == x"mac," ]; then
 		echo "Compiling for OSX"
