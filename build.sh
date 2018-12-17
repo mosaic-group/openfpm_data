cba2ca94657214b53079d8d08472ffcfed94519d
Pietro Incardona <incardon@mpi-cbg.de>
2018-12-17 16:28:18 +0100

    Fixing IO compilation on CI Farm

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-source $HOME/.bashrc
-source $HOME/openfpm_vars_$3
+
+workspace=$1
+hostname=$(hostname)
+branch=$3
+
+echo "Directory: $workspace"
+echo "Machine: $hostname"
+echo "Branch name: $branch"
 
 echo "Branch: $3"
-
-echo "$PATH"
 
 # Make a directory in /tmp/openfpm_data
 
@@ -20,14 +24,34 @@ git clone git@git.mpi-cbg.de:openfpm/openfpm_data.git openfpm_data
 git clone git@git.mpi-cbg.de:openfpm/openfpm_pdata.git openfpm_pdata
 git clone git@git.mpi-cbg.de:openfpm/openfpm_vcluster.git openfpm_vcluster
 
+if [ x"$hostname" == x"cifarm-centos-node.mpi-cbg.de"  ]; then
+        ./install_MPI_mpich.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+        ./install_BOOST.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	./install_HDF5.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	export PATH="$HOME/openfpm_dependencies/openfpm_io/$branch/MPI/bin/:$PATH"
+fi
+
+if [ x"$hostname" == x"cifarm-ubuntu-node.mpi-cbg.de"  ]; then
+        ./install_MPI_mpich.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	./install_BOOST.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	./install_HDF5.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	export PATH="$HOME/openfpm_dependencies/openfpm_io/$branch/MPI/bin/:$PATH"
+fi
+
+if [ x"$hostname" == x"cifarm-mac-node.mpi-cbg.de"  ]; then
+        export PATH="/usr/local/bin:$PATH"
+        ./install_MPI_mpich.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	./install_BOOST.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	./install_HDF5.sh $HOME/openfpm_dependencies/openfpm_io/$branch/ 4
+	export PATH="$HOME/openfpm_dependencies/openfpm_io/$branch/MPI/bin/:$PATH"
+fi
+
 # Go in the right branch
 
 
 cd "$1/openfpm_io"
 
 echo "Compiling on $2"
-
-source $HOME/openfpm_vars_$3
 
 sh ./autogen.sh
 if [ "$2" == "master" ]
@@ -39,9 +63,9 @@ then
  module load boost/1.54.0
  sh ./configure CXX=mpic++ --with-boost=/sw/apps/boost/1.54.0/ --with-hdf5=$HOME/$3/HDF5/bin/h5pcc
 else
- sh ./configure CXX=mpic++ --with-hdf5=$HOME/$3/HDF5/bin/h5pcc
+ sh ./configure CXX=mpic++ --with-hdf5=$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5 --with-boost=$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST
 fi
-make
+make -j 4
 
 if [ $? -ne 0 ]; then
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to compile the openfpm_io test \" }] }" https://hooks.slack.com/services/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
