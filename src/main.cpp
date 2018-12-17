321cd9016e530580c4b8c0e7329891697ffc0025
Pietro Incardona <incardon@mpi-cbg.de>
2018-12-18 00:32:59 +0100

    Fixing IO on CI Farm

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,22 @@
 
 #define BOOST_DISABLE_ASSERTS
 
-#define BOOST_TEST_MODULE "C++ test module for OpenFPM_io project"
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+
+// initialization function:
+bool init_unit_test()
+{
+  return true;
+}
+
+// entry point:
+int main(int argc, char* argv[])
+{
+  return boost::unit_test::unit_test_main( &init_unit_test, argc, argv );
+}
+
 
 #include "VCluster/VCluster.hpp"
 #include "unit_test_init_cleanup_io.hpp"
