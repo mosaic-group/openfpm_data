fd83af10181de2c1fc4ab5e7d9e606b06f6dbba1
Tommaso Bianucci <bianucci@mpi-cbg.de>
2019-06-05 15:24:46 +0200

    Update/add tests

--- src/SparseGridGpu/tests/SparseGridGpu_tests.cu
+++ src/SparseGridGpu/tests/SparseGridGpu_tests.cu
@@ -60,7 +60,6 @@ __global__ void insertValuesBlocked(SparseGridType sparseGrid)
 
     __syncthreads();
 
-//    (*(blocks[dataBlockNum]))[offset] = pos;
     blocks[dataBlockNum]->block[offset] = pos;
     BlockT::setElement(masks[dataBlockNum]->block[offset]);
 
@@ -107,21 +106,20 @@ BOOST_AUTO_TEST_SUITE(SparseGridGpu_tests)
         SparseGridGpu<AggregateT, 128> sparseGrid;
         sparseGrid.template setBackground<0>(666);
 
-        const unsigned int gridSize = 10;
-        const unsigned int blockSizeInit = 128; // Should be multiple of BlockT::size
+        const unsigned int gridSize = 3;
+        const unsigned int bufferPoolSize = 128; // Should be multiple of BlockT::size
         const unsigned int blockSizeInsert = 128;
         const unsigned int gridSizeRead = gridSize + 1;
         const unsigned int blockSizeRead = 128;
 
         // Prealloc insert buffer
-        sparseGrid.setGPUInsertBuffer(gridSize, blockSizeInit);
+        sparseGrid.setGPUInsertBuffer(gridSize, bufferPoolSize);
 
         // Initialize the insert buffer
         sparseGrid.initializeGPUInsertBuffer<0>();
 
         // Insert values
         insertValues<0> <<< gridSize, blockSizeInsert >>> (sparseGrid.toKernel());
-//        insertValuesBlocked<0, 2> <<< gridSize, blockSizeInsert >>> (sparseGrid.toKernel());
 
         // Flush inserts
         mgpu::ofp_context_t ctx;
@@ -154,7 +152,66 @@ BOOST_AUTO_TEST_SUITE(SparseGridGpu_tests)
         BOOST_REQUIRE_EQUAL(match, true);
     }
 
-//todo: Write a test that actually uses sparseGridGpu, to test the whole workflow
+    BOOST_AUTO_TEST_CASE(testInsert_blocked)
+    {
+        typedef aggregate<DataBlock<float, 64>> AggregateT;
+        typedef aggregate<float> AggregateOutT;
+        SparseGridGpu<AggregateT, 128> sparseGrid;
+        sparseGrid.template setBackground<0>(666);
+
+        const unsigned int gridSize = 3;
+        const unsigned int bufferPoolSize = 4; // Should be multiple of BlockT::size
+        const unsigned int blockSizeInsert = 128;
+        const unsigned int gridSizeRead = gridSize + 1;
+        const unsigned int blockSizeRead = 128;
+
+////////// DEBUG
+//        const unsigned int gridSize = 2;
+//        const unsigned int bufferPoolSize = 128; // Should be multiple of BlockT::size
+//        const unsigned int blockSizeInsert = 64;
+//        const unsigned int gridSizeRead = gridSize + 1;
+//        const unsigned int blockSizeRead = 64;
+//////////
+
+        // Prealloc insert buffer
+        sparseGrid.setGPUInsertBuffer(gridSize, bufferPoolSize);
+
+        // Initialize the insert buffer
+        sparseGrid.initializeGPUInsertBuffer<0>();
+
+        // Insert values
+        insertValuesBlocked<0, 2> <<< gridSize, blockSizeInsert >>> (sparseGrid.toKernel());
+
+        // Flush inserts
+        mgpu::ofp_context_t ctx;
+        sparseGrid.flush<smax_<0>>(ctx, flush_type::FLUSH_ON_DEVICE);
+
+        // Get output
+        openfpm::vector_gpu<AggregateOutT> output;
+        output.resize(gridSizeRead * blockSizeRead);
+
+        copyBlocksToOutput<0> <<< gridSizeRead, blockSizeRead >>> (sparseGrid.toKernel(), output.toKernel());
+
+        output.template deviceToHost<0>();
+        sparseGrid.template deviceToHost<0>();
+
+        // Compare
+        bool match = true;
+        for (size_t i = 0; i < output.size(); i++)
+        {
+            auto expectedValue = (i < gridSize * blockSizeInsert) ? i : 666;
+            std::cout << "sparseGrid(" << i << ") = " << sparseGrid.template get<0>(i)
+                      << " == "
+                      << expectedValue
+                      << " == "
+                      << output.template get<0>(i) << " = output(" << i << ")"
+                      << std::endl;
+            match &= output.template get<0>(i) == sparseGrid.template get<0>(i);
+            match &= output.template get<0>(i) == expectedValue;
+        }
+
+        BOOST_REQUIRE_EQUAL(match, true);
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
 
