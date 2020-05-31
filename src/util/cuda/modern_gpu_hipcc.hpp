51004768f74be8e0d5af2246541764a0c0fc39c1
Pietro Incardona <incardon@mpi-cbg.de>
2020-05-31 04:19:08 +0200

    Adding missing files

---
+++ src/util/cuda/modern_gpu_hipcc.hpp
@@ -0,0 +1,19 @@
+#ifndef MODERN_GPU_HIPCC_HPP__
+#define MODERN_GPU_HIPCC_HPP__
+
+namespace mgpu
+{
+	template<typename key_t>
+	struct plus_t
+	{};
+
+	template<typename key_t>
+        struct less_t
+        {};
+
+	template<typename key_t>
+        struct greater_t
+        {};
+}
+
+#endif
