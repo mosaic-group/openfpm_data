6b12f2fb8808a0e416bdbf59622c05eafe13801d
Pietro Incardona <incardon@mpi-cbg.de>
2020-06-11 00:16:17 +0200

    Fixing build

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ then
 fi
 
 if [ x"$hostname" == x"cifarm-centos-node.mpi-cbg.de"  ]; then
+	echo "CENTOS"
         export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5/lib"
 	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST/lib"
 fi
@@ -24,6 +25,7 @@ if [ x"$hostname" == x"cifarm-ubuntu-node"  ]; then
 fi
 
 if [ x"$hostname" == x"cifarm-mac-node.mpi-cbg.de"  ]; then
+	echo "MACOS X"
         export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5/lib"
 	export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST/lib"
 fi
