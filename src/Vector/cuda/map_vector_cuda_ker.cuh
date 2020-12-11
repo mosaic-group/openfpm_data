2ba69f0cdaf831e362bf7eafeead899df924662e
Pietro Incardona <incardon@mpi-cbg.de>
2020-12-11 20:45:44 +0100

    Fixing ghost put

--- src/Vector/cuda/map_vector_cuda_ker.cuh
+++ src/Vector/cuda/map_vector_cuda_ker.cuh
@@ -62,8 +62,6 @@ __global__ void merge_add_prp_device_impl_src_offset_dst_opar(vector_src_type v_
 	if (i >= opart.size())
 	{return;}
 
-	typedef typename vector_src_type::value_type TT;
-
 	// write the object in the last element
 	object_si_di_op<op,decltype(v_src.get(0)),decltype(v_dst.get(0)),OBJ_ENCAP,args...>(v_src.get(start + i),v_dst.get(opart.template get<0>(i)));
 }
