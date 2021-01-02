7983fcf10004e9683a8db2c51af218e6893d58cc
Pietro Incardona <incardon@mpi-cbg.de>
2021-01-02 16:40:48 +0100

    Removing garbage

--- build.sh
+++ build.sh
@@ -68,22 +68,8 @@ fi
 echo "Compiling on $2"
 
 sh ./autogen.sh
-if [ "$2" == "master" ]
-then
- sh ./configure CXX=mpic++ --with-hdf5=$HOME/$3/HDF5/bin/h5pcc --disable-gpu
-elif [ "$2" == "gin" ]
-then
- module load gcc/4.8.2
- module load boost/1.54.0
- sh ./configure CXX=mpic++ --with-boost=/sw/apps/boost/1.54.0/ --with-hdf5=$HOME/$3/HDF5/bin/h5pcc
-else
- sh ./configure CXX=mpic++ --with-hdf5=$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5 --with-boost=$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST --with-pdata=../../openfpm_pdata/
-fi
+sh ./configure CXX=mpic++ --with-hdf5=$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5 --with-boost=$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST --with-pdata=../../openfpm_pdata/
 
 make VERBOSE=1 -j 4
 
-if [ $? -ne 0 ]; then
-   curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to compile the openfpm_io test \" }] }" https://hooks.slack.com/services/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
-   exit 1 ; 
-fi
 
