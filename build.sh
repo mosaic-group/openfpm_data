b6a1f7e992e9f72ce18e83153202c0ab67e8ab94
Pietro Incardona <incardon@mpi-cbg.de>
2021-01-06 11:14:14 +0100

    Adding libhilbert and VCdevel

--- build.sh
+++ build.sh
@@ -74,7 +74,7 @@ fi
 echo "Compiling on $2"
 
 sh ./autogen.sh
-sh ./configure CXX=mpic++ --with-hdf5=$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5 --with-boost=$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST --with-pdata=../../openfpm_pdata/ --enable-cuda-on-cpu
+sh ./configure CXX=mpic++ --with-vcdevel=$HOME/openfpm_dependencies/openfpm_io/$branch/VCDEVEL --with-libhilbert=$HOME/openfpm_dependencies/openfpm_io/$branch/LIBHILBERT --with-hdf5=$HOME/openfpm_dependencies/openfpm_io/$branch/HDF5 --with-boost=$HOME/openfpm_dependencies/openfpm_io/$branch/BOOST --with-pdata=../../openfpm_pdata/ --enable-cuda-on-cpu
 
 make VERBOSE=1 -j 4
 
