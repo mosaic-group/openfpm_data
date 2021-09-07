e8106518c88200ab42939623fee6a703d785d494
Incardona Pietro <incardon@mpi-cbg.de>
2021-09-07 11:13:24 +0200

    Adding const attribute

--- src/util/performance/performance_util.hpp
+++ src/util/performance/performance_util.hpp
@@ -14,7 +14,7 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <fstream>
 
-static void addUpdtateTime(GoogleChart & cg, int np, std::string & base, std::string & filename)
+static void addUpdtateTime(GoogleChart & cg, int np, const std::string & base, const std::string & filename)
 {
     time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
