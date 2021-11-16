2537c681c4bd25f05e6a0e47e68b2822e74e6c8e
Incardona Pietro <incardon@mpi-cbg.de>
2021-11-16 14:29:29 +0100

    Reinstalling BOOST

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ fi
 if [ x"$hostname" == x"cifarm-mac-node.mpi-cbg.de"  ]; then
         echo "Mac node"
         export PATH="$HOME/openfpm_dependencies/openfpm_data/CMAKE/bin:$PATH"
+	rm -rf $HOME/openfpm_dependencies/openfpm_data/BOOST
 fi
 
 # Check if libHilbert is installed
